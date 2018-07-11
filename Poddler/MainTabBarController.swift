0a6716dc63e3cc3c9232b1fe0e7cc0c5884d951a
Sven Pohle <spohle@gmail.com>
2018-07-11 15:24:04 -0700

    Initial Commit

---
+++ Poddler/MainTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  MainTabBarController.swift
+//  Poddler
+//
+//  Created by Pohle, Sven on 7/11/18.
+//  Copyright © 2018 Pohle, Sven. All rights reserved.
+//
+
+import UIKit
+
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.tintColor = .purple
+        
+        viewControllers = [
+            generateNavigationController(viewController: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(viewController: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(viewController: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    fileprivate func generateNavigationController(viewController: UIViewController, title: String, image: UIImage) -> UINavigationController {
+        
+        viewController.navigationItem.title = title
+        
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        
+        return navController
+    }
+}
