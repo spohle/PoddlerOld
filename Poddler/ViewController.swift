e6188152b0c9605c2db73b6c0d682aa8155d1870
Sven Pohle <spohle@gmail.com>
2018-07-11 14:30:57 -0700

    Initial Commit

---
+++ Poddler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Poddler
+//
+//  Created by Pohle, Sven on 7/11/18.
+//  Copyright © 2018 Pohle, Sven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
